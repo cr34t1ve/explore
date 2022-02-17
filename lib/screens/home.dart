2b43430b21a2c4e933bddc64cee3a6b5111f7710
cr34t1ve <mrsofua@gmail.com>
2022-02-17 23:59:03 +0000

    size config

---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Hoome extends StatefulWidget {
+  const Hoome({Key? key}) : super(key: key);
+
+  @override
+  _HoomeState createState() => _HoomeState();
+}
+
+class _HoomeState extends State<Hoome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
